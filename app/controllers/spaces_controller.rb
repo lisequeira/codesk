2a6972f3774674618104911f05a561977c69674b
Liliana Sequeira <lilianasrsequeira@gmail.com>
2017-05-23 11:12:47 +0100

    CRUD Started

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,8 +1,37 @@
 class SpacesController < ApplicationController
 
+  before_action :set_space, except: [:new, :create]
 
-private
-def product_params
-  params.require(:product).permit(:name, :description, :photo)
-end
+  def index
+    @spaces = Spaces.all
+  end
+
+  def create
+    @space = Space.new(space_params)
+
+  end
+
+  def new
+
+    @space = Space.new
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+
+  private
+  def space_params
+    params.require(:product).permit(:address, :name, :photo, :type, :description, :country, :city, :rate, :user)
+  end
+
+  def set_space
+    @space = Space.find(params[:user_id])
+  end
 end
