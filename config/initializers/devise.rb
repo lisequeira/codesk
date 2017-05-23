eeeca9670451e29f929856915b5467fa3e08338d
Filipe Frazao <filipefrancof@gmail.com>
2017-05-23 13:56:03 +0100

    removed name attribute from users

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
 
   config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
     scope: 'email',
-    info_fields: 'first_name, last_name, email',
+    info_fields: 'email, first_name, last_name',
     image_size: 'square',  # 50x50, guaranteed ratio
     secure_image_url: true
 
