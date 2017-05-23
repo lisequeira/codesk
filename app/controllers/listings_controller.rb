5f0d37c0f963a5aaf60a601fda7b84d05f04de8c
Liliana Sequeira <lilianasrsequeira@gmail.com>
2017-05-23 16:20:08 +0100

    Date Picker New Listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,15 @@
 class ListingsController < ApplicationController
+
+  def new
+
+  end
+
+
 end
+
+ # t.bigint "space_id"
+ #    t.datetime "start_date"
+ #    t.datetime "end_date"
+ #    t.datetime "created_at", null: false
+ #    t.datetime "updated_at", null: false
+ #    t.index ["space_id"], name: "index_listings_on_space_id"
