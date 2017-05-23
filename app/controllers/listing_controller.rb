3e37de1840f6a1abb4381b55269f91469c0a4bfa
Liliana Sequeira <lilianasrsequeira@gmail.com>
2017-05-23 20:44:11 +0100

    Date Picker working, rest of the form missing

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -1,6 +1,25 @@
 class ListingController < ApplicationController
+  before_action :set_space, except: [:destroy]
 
   def new
     @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      redirect_to space_listing_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def listing_params
+
+  end
+
+  def set_space
+    @space = Space.find(params[:space_id])
   end
 end
