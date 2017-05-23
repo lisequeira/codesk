a1e08f32d402b260a576feac057126fc04dc6bab
Filipe Frazao <filipefrancof@gmail.com>
2017-05-23 18:07:02 +0100

    added user actions to controller and respective views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,26 @@
 class UsersController < ApplicationController
 
-  def index
+  before_action :set_space, only: [ :show, :edit, :update]
 
+  def show
   end
+
+  def update
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  def edit
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :address, :city, :birth_date)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
+
 end
