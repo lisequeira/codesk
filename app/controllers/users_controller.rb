0968f56124d587d1e654f13b5565164646537c97
Filipe Frazao <filipefrancof@gmail.com>
2017-05-23 18:32:34 +0100

    added link to profile. added profile data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,12 @@
-class UsersController < ApplicationController
+# class UsersController < ApplicationController
 
-  before_action :set_space, only: [ :show, :edit, :update]
 
-  def show
-  end
 
-  def update
-    @user.update(user_params)
-    redirect_to user_path(@user)
-  end
 
-  def edit
-  end
+# I don't think we need a Users controller.
+# Devise takes care of everything
 
-private
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :address, :city, :birth_date)
-  end
 
-  def set_user
-    @user = User.find(params[:id])
-  end
-end
 
-end
+
+# end
