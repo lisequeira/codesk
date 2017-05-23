8bef8f755361359713cb12ef85836ea6d944bab0
Sara de Praetere <saradepraetere@MacBook-Pro-2.local>
2017-05-23 15:03:36 +0100

    facebook button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523071239) do
+ActiveRecord::Schema.define(version: 20170523125407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,7 +87,6 @@ ActiveRecord::Schema.define(version: 20170523071239) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.string "city"
     t.date "birth_date"
     t.string "provider"
