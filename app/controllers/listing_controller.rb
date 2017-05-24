425b2c833576dbbe2de32d94fe0baa64d6286219
Liliana Sequeira <lilianasrsequeira@gmail.com>
2017-05-24 12:34:02 +0100

    Adding listings done

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -1,6 +1,7 @@
 class ListingController < ApplicationController
   before_action :set_space, except: [:destroy]
 
+
   def new
     @listing = Listing.new
   end
@@ -8,15 +9,20 @@ class ListingController < ApplicationController
   def create
     @listing = Listing.new(listing_params)
     if @listing.save
-      redirect_to space_listing_path
+      redirect_to space_listing_path(@space, @listing)
     else
       render 'new'
     end
   end
 
+  def show
+    @listing = Listing.find(params[:id])
+
+  end
+
   private
   def listing_params
-
+    params.require(:listing).permit(:id, :start_date, :end_date, :space_id)
   end
 
   def set_space
