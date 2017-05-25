e51a56687d108de7e48488728b6c9e09ae18d32a
Liliana Sequeira <lilianasrsequeira@gmail.com>
2017-05-25 16:23:26 +0100

    booking listing only allowed on available dates

--- app/models/space.rb
+++ app/models/space.rb
@@ -2,6 +2,7 @@ class Space < ApplicationRecord
   TYPE_SPACE = ["Desk", "Office", "Meeting Room", "Events Room"]
   belongs_to :user
   belongs_to :rate
+  has_many :listings
 
   has_attachment :photo
   accepts_nested_attributes_for :rate
